02e64635ecba58f616b6cdb065feaa6d2941902e
Shivam Sharma <shvmsrma@gmail.com>
2018-10-16 13:01:26 +0530

    New Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule}from '@angular/platform-browser/animations';
+import{MatToolbarModule} from '@angular/material/toolbar';
+import{FlexLayoutModule} from '@angular/flex-layout';
+import 'hammerjs';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
